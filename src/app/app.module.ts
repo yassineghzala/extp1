36bc1b249d2084c6a8ce92bb94eef5c2b682692b
yassineghzala <ghzalayassin@gmail.com>
2023-09-17 20:59:26 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BasesComponent } from './bases/bases.component';
+import { HeaderComponent } from './Website/header/header.component';
+import { FooterComponent } from './Website/footer/footer.component';
+import { ContentComponent } from './Website/content/content.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    BasesComponent
   ],
   imports: [
     BrowserModule,
